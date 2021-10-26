c6eabd447dab03dff553f09f5bd1fffb03114d14
Edilberto <edilbertobetinho93@gmail.com>
2021-10-25 23:03:07 -0300

    Update App.jsx

--- demanda1/src/App.jsx
+++ demanda1/src/App.jsx
@@ -5,50 +5,58 @@ import dadosUsina from './dadosUsina.json'
 class App extends Component {
 
   render() {
-    var strUser = document.getElementById("seletor").getElementsByTagName('option')
-    
-    strUser = dadosUsina.map(props => {
-      return props.strUser 
+    var selectElem = document.getElementById("seletor")
+    var pElem= document.getElementsByTagName('option')
+    selectElem.addEventListener('change', function() {
+      var index = selectElem.selectedIndex;
+      // Add that data to the <p>
+      pElem.innerHTML = 'selectedIndex: ' + index;
     })
-    console.log(strUser)
+    pElem = dadosUsina.map(props => {
+      if (pElem === props.tensao_V) {
+        props = pElem
+      }
+      return pElem
+    })
+    console.log(pElem)
     return (
       <div>
         <select id="seletor">
-          <option>tempo_h</option>
-          <option>tensao_V</option>
-          <option>corrente_A</option>
-          <option>potencia_kW</option>
-          <option>temperatura_C</option>
-          
+          <option value="tempo_h">tempo_h</option>
+          <option value="tensao_V">tensao_V</option>
+          <option value="corrente_A">corrente_A</option>
+          <option value="potencia_kW">potencia_kW</option>
+          <option value="temperatura_C">temperatura_C</option>
+
         </select>
-      
-      
-      {/* Line é o tipo de gráfico do react-chartjs-2*/}
-      <Line
+
+
+        {/* Line é o tipo de gráfico do react-chartjs-2*/}
+        <Line
           //Quando usa duas chaves
           //é objeto.
           data={{
-              //Labels são os valores de x no gráfico.
-              
-              labels: strUser,
-              //Personalizar os dados.
-              datasets: [{
-                  label: 'Tempo em Horas',
-                  data: strUser,
-                  fill: false,
-                  borderColor: 'rgb(75, 192, 192)',
-                  tension: 0.1,
-                  backgroundColor: "rgba(52,152,219,0.4)"
-              }]
+            //Labels são os valores de x no gráfico.
+
+            labels: pElem,
+            //Personalizar os dados.
+            datasets: [{
+              label: 'Tempo em Horas',
+              data: pElem,
+              fill: false,
+              borderColor: 'rgb(75, 192, 192)',
+              tension: 0.1,
+              backgroundColor: "rgba(52,152,219,0.4)"
+            }]
 
           }}
           //Tamanho
           height={100}
           //Largura
           width={300}
-      />
-      
-  </div>
+        />
+
+      </div>
     )
 
   }
