e9336144057ab8d86bde50e9e48a4011e09383ac
Edilberto <edilbertobetinho93@gmail.com>
2021-10-25 21:32:55 -0300

    Grafico 1
    Grafico

--- demanda1/src/App.jsx
+++ demanda1/src/App.jsx
@@ -1,14 +1,56 @@
-import Dadotempoh from "./Pages/Dadotempoh";
-import './App.css'
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import React from "react";
-const App = () => {
-    return (
-        <Router>
-            <Route path="/" exact component={App}/>
-            <Route path="/Pages/Dadostempoemhoras"  component={Dadotempoh}/>
-        </Router>
-    )
-}
+import { React, Component } from 'react';
+import './App';
+import { Line } from 'react-chartjs-2'
+import dadosUsina from './dadosUsina.json'
+class App extends Component {
 
-export default App;
+  render() {
+    var getValue = document.getElementById('seletor')
+
+    console.log(getValue)
+    getValue = dadosUsina.map(props => {
+      return props.getValue
+    })
+    return (
+      <div>
+        <select id="seletor">
+          <option value="tempo_h">tempo_h</option>
+          <option value="tensao_V">tensao_V</option>
+          <option value="corrente_A">corrente_A</option>
+          <option value="potencia_kW">potencia_kW</option>
+          <option value="temperatura_C">temperatura_C</option>
+          
+        </select>
+      
+      
+      {/* Line é o tipo de gráfico do react-chartjs-2*/}
+      <Line
+          //Quando usa duas chaves
+          //é objeto.
+          data={{
+              //Labels são os valores de x no gráfico.
+              
+              labels: getValue.slice(0,151),
+              //Personalizar os dados.
+              datasets: [{
+                  label: 'Tempo em Horas',
+                  data: getValue,
+                  fill: false,
+                  borderColor: 'rgb(75, 192, 192)',
+                  tension: 0.1,
+                  backgroundColor: "rgba(52,152,219,0.4)"
+              }]
+
+          }}
+          //Tamanho
+          height={100}
+          //Largura
+          width={300}
+      />
+      
+  </div>
+    )
+
+  }
+}
+export default App
