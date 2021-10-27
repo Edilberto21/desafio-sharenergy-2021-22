416a768caf08e168e1a55b7e33e28bae331fc579
Edilberto <edilbertobetinho93@gmail.com>
2021-10-27 00:10:18 -0300

    Update App.jsx

--- demanda1/src/App.jsx
+++ demanda1/src/App.jsx
@@ -1,61 +1,73 @@
-import { React, Component } from 'react';
-import './App';
-import { Line } from 'react-chartjs-2'
+import React, { useState } from 'react';
+import { Line } from 'react-chartjs-2';
 import dadosUsina from './dadosUsina.json'
-class App extends Component {
-
-  render() {
-    var select = document.getElementById("seletor")
-    var option = document.getElementsByTagName("option")
-    var opcaoValor = select.options[select.selectedIndex].value;
-    console.log(opcaoValor)
-    const dados = dadosUsina.map(props => {
-      if (opcaoValor === props.tempo_h) {
-        dados = props.tempo_h
-      }
-      return dados
-    })
-    console.log(opcaoValor)
+function Example() {
   
-    return (
-      <div>
-        <select id="seletor">
-          <option value="tempo_h">tempo_h</option>
-          <option value="tensao_V">tensao_V</option>
-          <option value="corrente_A">corrente_A</option>
-          <option value="potencia_kW">potencia_kW</option>
-          <option value="temperatura_C">temperatura_C</option>
+  const [selectValue, setSelectValue] = useState(1);  
+  //Lista de valores do select:
+  const list = [
+    {id: 'Selecione a variável', name: 'Selecione a variável'},
+    {id: 'tempo_h', name: 'tempo_h'},
+    {id: 'tensao_V', name: 'tensao_V'},
+    {id: 'corrente_A', name: 'corrente_A'},
+    {id: 'potencia_kW', name: 'potencia_kW'},
+    {id: 'temperatura_C', name: 'temperatura_C'},
+  ];
+  const dados = dadosUsina.map(props => {
+    if (list[1].id === selectValue) {
+        return props.tempo_h
+      } else if(list[2].id === selectValue){
+        return props.tensao_V
+      } else if(list[3].id === selectValue){
+        return props.corrente_A
+      } else if(list[4].id === selectValue){
+        return props.potencia_kW
+      } else if(list[5].id === selectValue){
+        return props.temperatura_C
+      } else {
+        return 'Selecione a variável'
+      }
 
-        </select>
+  })
+  return (    
+    
+    <div>
+      {/* value está recebendo o valor selecionado.
+      (e) é o evento.
+      onchange é um método que depois que há alguma 
+      mudança executa determinada ação.
+      list.map() manipulando a lista,
+      pegando o item.id que são os ids da listas.
+      pegando o item.name que são os nomes das listas. 
+      */}
+      <select value={selectValue} onChange={e => setSelectValue(e.target.value)}>
+        {list.map((item) => (
+          <option value={item.id}>{item.name}</option>
+        ))}        
+      </select>
 
-        {/* Line é o tipo de gráfico do react-chartjs-2*/}
-        <Line
+      <Line
           //Quando usa duas chaves
           //é objeto.
           data={{
             //Labels são os valores de x no gráfico.
-
             labels: dados,
             //Personalizar os dados.
             datasets: [{
-              label: 'Tempo em Horas',
               data: dados,
-              fill: false,
-              borderColor: 'rgb(75, 192, 192)',
-              tension: 0.1,
-              backgroundColor: "rgba(52,152,219,0.4)"
-            }]
-
+              //Nome do gráfico:
+              label: 'Gráficos',
+              borderColor: 'rgb(0,0,139)',
+              backgroundColor: "rgb(6,251,255)"
+            }],
           }}
           //Tamanho
           height={100}
           //Largura
           width={300}
         />
-
-      </div>
-    )
-    
-  }
+    </div>
+  )
 }
-export default App
+
+export default Example;
